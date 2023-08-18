79fec54299845d139741770dde1e417bc9ec39f4
dilution0216 <hglad0216@naver.com>
2023-08-18 21:08:36 +0900

    <Repository> AccumullatedTimeRepository 생성 AccumulatedTime 엔티티와 연결, 생성해야 할 메서드 주석

--- src/main/java/com/example/swithme/repository/AccumullatedTimeRepository.java
+++ src/main/java/com/example/swithme/repository/AccumullatedTimeRepository.java
@@ -4,4 +4,5 @@ import com.example.swithme.entity.AccumulatedTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccumullatedTimeRepository extends JpaRepository<AccumulatedTime,Long> {
+    // 누적시간으로 내림차순 정렬하는 메서드 생성하기
 }
