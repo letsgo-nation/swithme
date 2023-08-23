9b464e8daa360d0bab5feaac0d6b86e761131332
jangwon kim <seed0335@gmail.com>
2023-08-23 15:10:01 +0900

    관리자 페이지 구현

--- src/main/java/com/example/swithme/controller/ManageController.java
+++ src/main/java/com/example/swithme/controller/ManageController.java
@@ -1,19 +1,27 @@
 package com.example.swithme.controller;
 
+import com.example.swithme.dto.MyStudyResponseDto;
 import com.example.swithme.entity.UserRoleEnum;
 import com.example.swithme.security.UserDetailsImpl;
+import com.example.swithme.service.ManagerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @Secured(UserRoleEnum.Authority.ADMIN) // 관리자만 접근 가능
 public class ManageController {
 
+    ManagerService managerService;
 
-    @GetMapping("/products/secured")
+    @GetMapping("/secured")
     public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
         for (GrantedAuthority authority : userDetails.getAuthorities()) {
@@ -22,5 +30,10 @@ public class ManageController {
         return "user/manager";
     }
 
-    //조회
+    // 전체 개인 스터디 게시물 조회
+    @GetMapping("/secured/myStudies")
+    public String getMyStudies() {
+        managerService.findAll();
+        return "user/manager";
+    }
 }
