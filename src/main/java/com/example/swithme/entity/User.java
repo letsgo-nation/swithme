c1c55e7c6f61011e977fea1c417d327459ab493d
mbc2579 <mbc2579@naver.com>
2023-08-25 12:05:01 +0900

    USER entity

--- src/main/java/com/example/swithme/entity/User.java
+++ src/main/java/com/example/swithme/entity/User.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Setter
 @Getter
@@ -31,6 +33,9 @@ public class User {
     private Long kakaoId;
 
     private String googleId;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<GroupUser> groupUsers;
 
     public User(String username, String password, String nickname) {
         this.username = username;
