0224bad055dd4dd331a9512901bfe2d266db659c
dilution0216 <hglad0216@naver.com>
2023-08-25 12:08:07 +0900

    feat : Add 누적시간 Repository 누적시간 레포지토리 구현

--- src/main/java/com/example/swithme/repository/AccumulatedTimeRepository.java
+++ src/main/java/com/example/swithme/repository/AccumulatedTimeRepository.java
@@ -2,7 +2,9 @@ package com.example.swithme.repository;
 
 import com.example.swithme.entity.AccumulatedTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AccumullatedTimeRepository extends JpaRepository<AccumulatedTime,Long> {
+@Repository
+public interface AccumulatedTimeRepository extends JpaRepository<AccumulatedTime,Long> {
     // 누적시간으로 내림차순 정렬하는 메서드 생성하기
 }
