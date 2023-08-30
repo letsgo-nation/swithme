e9b87b3c68c3f372543fb7192790f6d99734c9ea
letsgo-nation <ssowon528@gmail.com>
2023-08-31 01:04:39 +0900

    modify: "timestamped format"
    시간형태를 yyyy-MM-dd HH:mm:ss로 수정,
    posts.html 게시물 작성 시간 형태 변경

--- src/main/java/com/example/swithme/dto/PostResponseDto.java
+++ src/main/java/com/example/swithme/dto/PostResponseDto.java
@@ -13,21 +13,23 @@ public class PostResponseDto {
     private Long id;
     private String title;
     private String content;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
+    private String createdAt;
+    private String modifiedAt;
     private Long category_id;
     private Long user_id;
     private String username;
     private String userNickname;
+    private String category_name;
     private List<CommentResponseDto> commentResponseDtoList;
 
     public PostResponseDto(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
-        this.createdAt = post.getCreatedAt();
-        this.modifiedAt = post.getModifiedAt();
+        this.createdAt = post.getCreatedAtFormatted();
+        this.modifiedAt = post.getModifiedAtFormatted();
         this.category_id = post.getCategory().getId();
+        this.category_name = post.getCategory().getName();
         this.user_id = post.getUser().getUserId(); // post ->  User -> userId
         this.username = post.getUser().getUsername(); // post ->  User -> username
         this.userNickname = post.getUser().getNickname();
