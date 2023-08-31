197f46ca3751036fca6967e2c5200b361f7572c6
letsgo-nation <ssowon528@gmail.com>
2023-09-01 01:17:40 +0900

    add: "postUpdate 조회"

--- src/main/resources/static/js/post/postDetail.js
+++ src/main/resources/static/js/post/postDetail.js
@@ -1,19 +1,4 @@
 // 삭제 버튼 클릭 시 게시물 삭제
-
-// let postId = [[${post.id}]]
-// function deletePost() {
-//     $.ajax({
-//         type:'DELETE',
-//         url:'/api/post/' + postId,
-//         success: function(response) {
-//             alert("게시글이 삭제되었습니다");
-//             window.location.href = "/view/posts";
-//         }, error: function(req,status,error) {
-//             alert("게시글 삭제가 실패하였습니다");
-//         }
-//     })
-// }
-
 document.getElementById("deleteButton").addEventListener("click", function() {
     let postId = this.getAttribute("data-postid");
 
---
+++ src/main/resources/static/js/post/postUpdate.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var modeSwitch = document.querySelector('.mode-switch');
+
+    modeSwitch.addEventListener('click', function () {
+        document.documentElement.classList.toggle('dark');
+        modeSwitch.classList.toggle('active');
+    });
+
+// 카테고리 불러오기
+    $.ajax({
+        type: 'GET',
+        url: '/api/categories',
+        success: function(response) {
+            let form_category = $('#category');
+            form_category.empty();
+            let data = response['data'];
+            for(let i=0;i<data.length;i++) {
+                let category = data[i]
+                let category_id = category['id'];
+                let name = category['name'];
+                let option_value = i+1;
+                form_category.append(`<option value=${option_value}>${name}</option>`);
+            }
+        }
+    })
+});
