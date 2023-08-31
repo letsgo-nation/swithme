a932d778aa51fd8a4d7718bd8c1a55d2b85af1f8
letsgo-nation <ssowon528@gmail.com>
2023-08-31 16:43:34 +0900

    add: "postDetail 게시물 상세 페이지 API"

--- src/main/java/com/example/swithme/controller/PostViewController.java
+++ src/main/java/com/example/swithme/controller/PostViewController.java
@@ -27,23 +27,20 @@ public class PostViewController {
         return "post/posts";
     }
 
-    // 게시물 작성 페이지
+    // 게시물 작성 페이지 - 작성자 닉네임 가져오기
     @GetMapping("/post/write")
     public String writePage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         model.addAttribute("info_username", userDetails.getUser().getNickname());
         return "post/postWrite";
     }
 
-//
-//
-//    // 게시글 상세 페이지
-//    @GetMapping("/post/detail/{id}")
-//    public String bringPost(@PathVariable Long id,
-//                            Model model) {
-//        PostResponseDto result = postService.getPost(id);
-//        model.addAttribute("post", result);
-//        return "postDetail";
-//    }
+    // 게시글 상세 페이지
+    @GetMapping("/post/detail/{id}")
+    public String postDetailPage(@PathVariable Long id, Model model) {
+        PostResponseDto result = postService.lookupPost(id);
+        model.addAttribute("post", result);
+        return "post/postDetail";
+    }
 //
 //    // 게시글 수정 페이지
 //    @GetMapping("/post/modify/{id}")
