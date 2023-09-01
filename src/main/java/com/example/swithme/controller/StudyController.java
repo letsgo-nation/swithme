b75d6111b73688ba652766268783b139046c8391
mbc2579 <mbc2579@naver.com>
2023-09-01 14:00:25 +0900

    캘린더 기능 - 개인 일정관리 캘린더

--- src/main/java/com/example/swithme/controller/StudyController.java
+++ src/main/java/com/example/swithme/controller/StudyController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StudyController {
@@ -40,6 +42,40 @@ public class StudyController {
         List<Study> studies = studyRepository.findAll();
         model.addAttribute("studies", studies);
         return "study/calendar";
+    }
+
+    @GetMapping("/studies/details/{id}")
+    public String showStudyDetails(@PathVariable Long id, Model model) {
+        Optional<Study> optionalStudy = studyRepository.findById(id);
+        if (optionalStudy.isPresent()) {
+            Study study = optionalStudy.get();
+            model.addAttribute("study", study);
+            return "study/details";
+        }
+        return "redirect:/studies/calendar";
+    }
+
+    @GetMapping("/studies/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        Optional<Study> optionalStudy = studyRepository.findById(id);
+        if (optionalStudy.isPresent()) {
+            Study study = optionalStudy.get();
+            model.addAttribute("study", study);
+            return "study/editForm";
+        }
+        return "redirect:/studies/calendar";
+    }
+
+    @PostMapping("/studies/update")
+    public String updateStudy(@ModelAttribute Study study) {
+        studyRepository.save(study);
+        return "redirect:/studies/calendar";
+    }
+
+    @GetMapping("/studies/delete/{id}")
+    public String deleteStudy(@PathVariable Long id) {
+        studyRepository.deleteById(id);
+        return "redirect:/studies/calendar";
     }
 }
 
