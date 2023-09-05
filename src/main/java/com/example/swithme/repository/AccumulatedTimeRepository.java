0d43048e88db05f3b03004b6c18058f1c795fa9a
dilution0216 <hglad0216@naver.com>
2023-09-05 12:14:19 +0900

    feat : AccumulatedTimeRepository 에 findByUser 메서드 추가

--- src/main/java/com/example/swithme/repository/AccumulatedTimeRepository.java
+++ src/main/java/com/example/swithme/repository/AccumulatedTimeRepository.java
@@ -4,7 +4,10 @@ import com.example.swithme.entity.AccumulatedTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccumulatedTimeRepository extends JpaRepository<AccumulatedTime,Long> {
     // 누적시간으로 내림차순 정렬하는 메서드 생성하기
+    List<AccumulatedTime> findByUser();
 }
