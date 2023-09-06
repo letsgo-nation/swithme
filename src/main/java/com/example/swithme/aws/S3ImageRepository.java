060c823bdc8bd0aa88c7df5da5300a8be0c57de7
letsgo-nation <ssowon528@gmail.com>
2023-09-06 21:01:49 +0900

    feat: "파일 삭제"

--- src/main/java/com/example/swithme/aws/S3ImageRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.example.swithme.aws;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface S3ImageRepository extends JpaRepository<S3Image, Long> {
-//}
